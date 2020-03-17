8dfa0c8fa10e085def9ba74333859698d24603cc
praveen kumar ch <61839011+DevSecops-praveen@users.noreply.github.com>
2020-03-17 16:30:49 +0530

    Update adduser.sh

--- adduser.sh
+++ adduser.sh
@@ -1,3 +1,3 @@
-aws iam create-user --user-name praveen01
-aws iam create-group --group-name jenkins01
-aws iam add-user-to-group --user-name praveen01 --group-name jenkins01
+aws iam create-user --user-name praveen@01
+aws iam create-group --group-name jenkins@01
+aws iam add-user-to-group --user-name praveen@01 --group-name jenkins@01
